79aac1e6956d23697201555236fbd8e976e0c0ea
Tom Li <tomleslieli@yahoo.com>
2022-07-15 17:48:22 -0400

    Initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/stylesheets/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='main-1'>
+        hi
+      </div>
+      <div className='main-2'>
+
+      </div>
     </div>
   );
 }
---
+++ src/javascripts/navbar.jsx
@@ -0,0 +1,40 @@
+import '../assets/stylesheets/navbar.scss';
+
+function NavBar() {
+    const subject = 'Software Engineer Position'
+    const body = 'Hello Tom! We would love to have you join our team at <COMPANY NAME GOES HERE>.'
+    return (
+        <div className='navbar'>
+            <div className='navbar-left'>
+                <div className='personal-logo'>
+                    Hi
+                </div>
+            </div>
+            <div className='navbar-right'>
+                <a href='https://linkedin.com/in/tomleslieli/'>
+                    <button>
+                        <div className='button-logo'>
+                            <img src='https://tomleslieli-portfolio.s3.amazonaws.com/linkedin-logo.png' alt='linkedin-logo'/>
+                        </div>
+                    </button>
+                </a>
+                <a href='https://github.com/tomleslieli'>
+                    <button>
+                        <div className='button-logo'>
+                            <img src='https://tomleslieli-portfolio.s3.amazonaws.com/github-logo.png' alt='github-logo'/>
+                        </div>
+                    </button>
+                </a>
+                <a href={`mailto:tomleslieli@yahoo.com?subject=${subject}&body=${body}`}>
+                    <button>
+                        <div className='button-logo'>
+                            <img src='https://tomleslieli-portfolio.s3.amazonaws.com/email-icon.png' alt='email-icon'/>
+                        </div>
+                    </button>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default NavBar;
